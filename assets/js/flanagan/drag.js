0ca665976244a47580cf00d7c6cbd36478ed11b2
Vova <mgrinevitch@gmail.com>
2018-01-29 16:30:05 +0200

    flanagan1

--- assets/js/flanagan/drag.js
+++ assets/js/flanagan/drag.js
@@ -2,4 +2,35 @@ function drag(elementToDrag, event) {
   var scroll = getScrollOffsets();
 
   var startX = event.clientX + scroll.x;
+  var startY = event.clientY + scroll.y;
+
+  var origX = elementToDrag.offsetLeft;
+  var origY = elementToDrag.offsetTop;
+
+  var deltaX = startX - origX;
+  var deltaY = startY - origY;
+
+  if (document.addEventListener) {
+    document.addEventListener("mousemove", moveHandler, true);
+    document.addEventListener("mouseup", upHandler, true);
+  }
+  if (event.stopPropagation()) event.stopPropagation();
+
+  if (event.preventDefault()) event.preventDefault();
+
+  function moveHandler(e) {
+    var scroll = getScrollOffsets();
+    elementToDrag.style.left = (e.clientX + scroll.x - deltaX) + "px";
+    elementToDrag.style.top = (e.clientY + scroll.y - deltaY) + "px";
+
+    if (e.stopPropagation) e.stopPropagation();
+  }
+
+  function upHandler(e) {
+    if (document.removeEventListener) {
+      document.removeEventListener("mouseup", upHandler, true);
+      document.removeEventListener("mousemove", moveHandler, true);
+    }
+    if (e.stopPropagation) e.stopPropagation();
+  }
 }
--- assets/js/main2.js
+++ assets/js/main2.js
@@ -2,11 +2,13 @@ list.addEventListener("click", function() {
   console.log(this)
 });
 
-document.querySelector("#list").children[0].addEventListener("contextmenu", function(e) {e.preventDefault(); console.log(this) });
+document.querySelector("#list").children[0].addEventListener("contextmenu", handler);
 
-document.querySelector("#list").children[0].addEventListener("click", function(e) {console.log(this) });
-
-document.querySelector("#list").children[0].addEventListener("mousedown", function(e) {console.log(this) });
 
 document.querySelector("span").addEventListener("click", function(e) {console.log(this) });
 
+
+function handler(event) {
+  event.preventDefault(); console.log(this);
+
+}
